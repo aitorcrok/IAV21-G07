19a7aa18a81b83d240a9c48a8594e6f496617157
David Godoy Ruiz <dagodoy@ucm.es>
2021-03-04 12:49:41 +0100

    Tenemos gameplay

--- P1/Assets/Scripts/Llegada.cs
+++ P1/Assets/Scripts/Llegada.cs
@@ -9,49 +9,51 @@
     /// </summary>
     public class Llegada : ComportamientoAgente
     {
-        public float tiempoAObjetivo = 0.25f;
-        public float margen = 0.1f;
-        //public float radioExt = 3.0f;
+        public float tiempoAObjetivo = 0.1f;
+        public float margen = 0.3f;
+        public float radioExt = 3.0f;
         public float velMaxima = 8;
-        //float velObjetivo;
-        //float distancia;
+        float velObjetivo;
+        float distancia;
         /// <summary>
         /// Obtiene la dirección
         /// </summary>
         /// <returns></returns>
-        public override Direccion GetDireccion()
-        {
-            Direccion direccion = new Direccion();
-            direccion.lineal = objetivo.transform.position - transform.position;
-
-            if (direccion.lineal.magnitude < margen)
-                return new Direccion();
-            direccion.lineal /= tiempoAObjetivo;
-
-            return direccion;
-        }
-
         //public override Direccion GetDireccion()
         //{
         //    Direccion direccion = new Direccion();
         //    direccion.lineal = objetivo.transform.position - transform.position;
-        //    distancia = direccion.lineal.magnitude;
-        //    direccion.lineal.Normalize();
 
-        //    if (distancia > radioExt)
-        //    {
-        //        velObjetivo = agente.aceleracionMax;
-        //    }
-        //    else if (distancia < radio)
-        //    {
-        //        velObjetivo = 0;
-        //    }
-        //    else
-        //    {
-        //        velObjetivo = agente.aceleracionMax * distancia / radioExt;
-        //    }
-        //    direccion.lineal = direccion.lineal * velObjetivo;
+        //    if (direccion.lineal.magnitude < margen)
+        //        return new Direccion();
+        //    direccion.lineal /= tiempoAObjetivo;
+
         //    return direccion;
         //}
+
+        public override Direccion GetDireccion()
+        {
+            Direccion direccion = new Direccion();
+            direccion.lineal = objetivo.transform.position - transform.position;
+            distancia = direccion.lineal.magnitude;
+            direccion.lineal.Normalize();
+
+            if (distancia > radioExt)
+            {
+                velObjetivo = agente.aceleracionMax;
+            }
+            else if (distancia < margen)
+            {
+                    velObjetivo = 0;
+            }
+            else
+            {
+                velObjetivo = agente.aceleracionMax * distancia / radioExt;
+            }
+            direccion.lineal = direccion.lineal * velObjetivo;
+            direccion.lineal = direccion.lineal - agente.velocidad;
+            direccion.lineal /= tiempoAObjetivo;
+            return direccion;
+        }
     }
 }
