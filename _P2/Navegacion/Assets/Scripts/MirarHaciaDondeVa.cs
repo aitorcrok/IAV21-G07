6943c2ffcd7d33bb0fa5a867ef8241ede1bc9474
David Godoy Ruiz <dagodoy@ucm.es>
2021-04-09 01:53:16 +0200

    Mirar hacia donde andas
    Dolor

---
+++ _P2/Navegacion/Assets/Scripts/MirarHaciaDondeVa.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UCM.IAV.Movimiento
+{
+
+    public class MirarHaciaDondeVa : Alinear
+    {
+        public override void Awake()
+        {
+            base.Awake();
+            objetivo = new GameObject();
+            objetivo.AddComponent<Agente>();
+        }
+
+        public override Direccion GetDireccion()
+        {
+            Vector3 dir = GetComponent<Rigidbody>().velocity;
+            if (dir.magnitude == 0)
+                return new Direccion();
+
+            objetivo.GetComponent<Agente>().orientacion = Mathf.Atan2(dir.x, dir.z);
+            return base.GetDireccion();
+        }
+    }
+
+}
+
+
