5a9cbd6566c9370100790534eb64d1d3ba3b8cab
Jorge <jorgbell@ucm.es>
2021-05-04 22:41:50 +0200

    casi hecho que la lampara se pueda tirar y salvar
    solo hace falta que la lampara se mueva de verdad pero la logica creo que esta hecha, chapucera pero esta

---
+++ _P3/IAVP3G07/Assets/Scripts/RestoreLampara.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+public class RestoreLampara : MonoBehaviour
+{
+    bool aire = true; //true = lampara en el aire
+    public float speed;
+    Vector3 originalPos;
+    public GameObject palanca;
+    public SceneVariables sv;
+    public string variableName;
+    private void Start()
+    {
+        originalPos = transform.position;
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (!aire && Input.GetKey(KeyCode.Space))
+        {
+            aire = true;
+            Debug.Log(variableName + " restaurada");
+            Variables.Object(sv).Set(variableName, true);
+        }
+    }
+    private void Update()
+    {
+        if (aire) {
+            if (transform.position.y < originalPos.y)
+                transform.position = originalPos;
+            else
+                palanca.GetComponent<Palanca>().setPalanca(false);
+        }    
+    }
+    public void setRestore(bool b) { aire = b; }
+}
