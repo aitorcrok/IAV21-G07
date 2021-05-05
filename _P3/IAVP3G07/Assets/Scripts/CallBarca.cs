1dc21a10dad312094334e858f9c41bb0d8427889
Jorge <jorgbell@ucm.es>
2021-05-05 13:01:10 +0200

    creadas las barcas putamadre dios increibles aunque solo hay un par

---
+++ _P3/IAVP3G07/Assets/Scripts/CallBarca.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallBarca : MonoBehaviour
+{
+    public Transform nextTarget;
+    public GameObject barca;
+    GoToTarget gtt;
+    private void Start()
+    {
+        gtt = barca.GetComponent<GoToTarget>();
+    }
+    // Start is called before the first frame update
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKey(KeyCode.Space))
+            gtt.setTarget(nextTarget);
+    }
+}
---
+++ _P3/IAVP3G07/Assets/Scripts/GoToTarget.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToTarget : MonoBehaviour
+{
+    public Transform initialTarget;
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, initialTarget.position, 3 * Time.deltaTime);
+    }
+
+    public void setTarget(Transform t) { initialTarget = t; }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        collision.gameObject.transform.SetParent(this.transform);
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        collision.gameObject.transform.SetParent(null);
+    }
+}
