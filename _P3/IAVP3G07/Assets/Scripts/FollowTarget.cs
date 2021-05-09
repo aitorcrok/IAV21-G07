da1768509f11211a4544eca3937f3e97b8d4a6c3
aitorcrok <aitorcrok>
2021-05-09 13:14:31 +0200

    improved camera

--- _P3/IAVP3G07/Assets/Scripts/FollowTarget.cs
+++ _P3/IAVP3G07/Assets/Scripts/FollowTarget.cs
@@ -4,18 +4,12 @@ using UnityEngine;
 
 public class FollowTarget : MonoBehaviour
 {
-    public GameObject target;
-    Vector3 deltaPos;
-
+    public Transform target;
     // Start is called before the first frame update
-    void Start()
-    {
-        deltaPos = transform.position - target.transform.position;
-    }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        transform.position = new Vector3(target.transform.position.x + deltaPos.x, transform.position.y, target.transform.position.z + deltaPos.z);
+        transform.position = target.position;
     }
 }
