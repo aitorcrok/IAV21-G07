04034c14dc61a9fe104dd3854fc3217639b9e8f6
aitorcrok <aitorcrok>
2021-05-09 18:24:06 +0200

    camera controller (V, C y F)

---
+++ _P3/IAVP3G07/Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Camera countCamera;
+    [SerializeField] Camera singerCamera;
+    [SerializeField] Camera ghostCamera;
+    int activeCam = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        if(activeCam != 0 && Input.GetKey(KeyCode.V))
+        {
+            countCamera.enabled = true;
+            singerCamera.enabled = false;
+            ghostCamera.enabled = false;
+            activeCam = 0;
+        }
+        else if (activeCam != 1 && Input.GetKey(KeyCode.C))
+        {
+            countCamera.enabled = false;
+            singerCamera.enabled = true;
+            ghostCamera.enabled = false;
+            activeCam = 1;
+        }
+        else if (activeCam != 2 && Input.GetKey(KeyCode.F))
+        {
+            countCamera.enabled = false;
+            singerCamera.enabled = false;
+            ghostCamera.enabled = true;
+            activeCam = 2;
+        }
+    }
+}
